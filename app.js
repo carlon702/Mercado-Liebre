0bcf68ec0f6cd9f1e0d28ac4c1601bb2a3190ec7
carlon702 <carlosj.marchal@gmail.com>
2022-08-15 13:38:32 -0300

    terminado estilos css

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const publicPath = path.join(__dirname, './public');
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
