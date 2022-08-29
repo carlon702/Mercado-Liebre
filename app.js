7c061e073c115f1bf825269eab077012902484f2
carlon702 <carlosj.marchal@gmail.com>
2022-08-29 18:54:37 -0300

    rehice mercadoliebre, estilos casi terminados

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
-});
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, './views/home.html'))
+})
 
-app.listen(3000, () => {
-    console.log('http://localhost:3000/')
-} );
+app.listen(3000, (req, res)=>{
+    console.log('listening at localhost:3000')
+})
