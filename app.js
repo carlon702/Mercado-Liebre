d9441a072795315eea2809fd29d84d953d06d065
carlon702 <carlosj.marchal@gmail.com>
2022-08-29 20:26:44 -0300

    actualizado para heroku

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, './views/home.html'))
 })
 
-app.listen(3000, (req, res)=>{
+app.listen(process.env.PORT || 3000, (req, res)=>{
     console.log('listening at localhost:3000')
 })
