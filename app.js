e92b7294a1a85547f172075e6f5eb5c6c0d06d44
carlon702 <carlosj.marchal@gmail.com>
2022-08-31 18:23:55 -0300

    rehice mercadoliebre

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, './views/home.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
 
-app.listen(3000, (req, res)=>{
-    console.log('listening at localhost:3000')
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.listen(3000, (req, res) => {
+  console.log("listening at localhost:3000");
+});
